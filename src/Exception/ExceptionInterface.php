fe4ab2ef71362d64291c0bf81557623cc6c956d1
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 13:48:01 -0500

    Update ExceptionInterface.php

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,6 +1,16 @@
 <?php
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/LaxovePHP/Encryption> Github Repository.
+ * @license <https://github.com/LaxovePHP/Encryption/blob/master/LICENSE> New BSD License.
+ */
+
 namespace Nenglish7\Encryption\Exception;
 
+/**
+ * ExceptionInterface
+ */
 interface ExceptionInterface
 {
 }
