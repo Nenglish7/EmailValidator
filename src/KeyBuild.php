be1c24e3abc26f3e0486adad20071433536a2a92
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 14:01:59 -0500

    Create KeyBuild.php

---
+++ src/KeyBuild.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/LaxovePHP/Encryption> Github Repository.
+ * @license <https://github.com/LaxovePHP/Encryption/blob/master/LICENSE> New BSD License.
+ */
+
+namespace Nenglish7\Lovell;
+
+/**
+ * KeyBuild.
+ */
+class KeyBuild extends KeyController implements KeyBuildInterface
+{
+}
