cfb9b9d23e5e85bd1b2ae767188244fd79bfd701
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 15:29:57 -0500

    Update XSSGuardExceptionTrait.php

--- src/Exception/XSSGuardExceptionTrait.php
+++ src/Exception/XSSGuardExceptionTrait.php
@@ -1,1 +1,32 @@
-
+<?php
+declare(strict_types=1);
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/Lovell> Github Repository.
+ * @license <https://github.com/Nenglish7/Lovell/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Nenglish7\Lovell\Exception;
+ 
+/**
+ * XSSGuardExceptionTrait.
+ */
+trait XSSGuardExceptionTrait
+{
+    
+    /**
+     * Protect against xss attacks.
+     *
+     * @param mixed $data      The data to convert.
+     * @param string $encoding The type of encoding to use.
+     *
+     * @return string.
+     */
+    public function xssProtect($data, string $encoding): string
+    {
+        $data = strval($data);
+        return htmlspecialchars($data, \ENT_QUOTES, $encoding);
+    }
+  
+}
