4dd6fcfc235c0fe7558908c149964d2a3a48f8a9
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 13:42:53 -0500

    Create boot.php
    Create a constant that tells you if the dependency has been installed correctly.

---
+++ boot.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/LaxovePHP/Encryption> Github Repository.
+ * @license <https://github.com/LaxovePHP/Encryption/blob/master/LICENSE> New BSD License.
+ */
+ 
+define('NENGLISH7_ENCRYPTION_PRE_LOADED', true);
