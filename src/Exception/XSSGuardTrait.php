f8a69d3858ea5e805bd67521c88c2eb8eba57d0b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 17:48:38 -0500

    Create XSSGuardTrait.php

---
+++ src/Exception/XSSGuardTrait.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/Lovell> Github Repository.
+ * @license <https://github.com/Nenglish7/Lovell/blob/master/LICENSE> New BSD License.
+ *
+ * @package Laxove\Security.
+ */
+ 
+namespace LaxovePHP\Security\Guard;
+ 
+/**
+ * XSSGuardTrait.
+ */
+trait XSSGuardTrait
+{
+    
+    use XSSGuardExceptionTrait;
+  
+}
