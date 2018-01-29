2fc376fabf8c6b6a2ca43ed1acb2b2a1a9a3344c
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 18:14:03 -0500

    Delete boot.php

--- boot.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @author Nicholas English <nenglish0820@outlook.com>.
- *
- * @link    <https://github.com/LaxovePHP/Encryption> Github Repository.
- * @license <https://github.com/LaxovePHP/Encryption/blob/master/LICENSE> New BSD License.
- */
- 
-define('NENGLISH7_LOVELL_PRE_LOADED', true);
