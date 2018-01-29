f681bfccbe743827fde10d3ae80afafb53355069
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 15:36:14 -0500

    Update KeyGuardException.php

--- src/Exception/KeyGuardException.php
+++ src/Exception/KeyGuardException.php
@@ -1,1 +1,35 @@
-
+<?php
+declare(strict_types=1);
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/Lovell> Github Repository.
+ * @license <https://github.com/Nenglish7/Lovell/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Nenglish7\Lovell\Exception;
+ 
+/**
+ * KeyGuardException.
+ */
+class KeyGuardException extends RuntimeException implements ExceptionInterface
+{
+  
+    use XSSGuardExceptionTrait;
+  
+    /**
+     * Constuct a custom key guard exception.
+     *
+     * @param string $message The message for the occurring error.
+     * @param int $code       The error code so the error can be looked up.
+     *
+     * @return void.
+     */
+    function __construct(string $message, int $code)
+    {
+        $message = $this->xssProtect($message);   
+        $code = $this->xssProtect($code);
+        parent::__construct(\sprintf('`%s` - `%s`', $message, $code));
+    }
+ 
+}
