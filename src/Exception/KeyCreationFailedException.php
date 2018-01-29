60dd442ca497f2e019a176b9afa3769dd8fc9e80
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 14:03:48 -0500

    Update KeyCreationFailedException.php

--- src/Exception/KeyCreationFailedException.php
+++ src/Exception/KeyCreationFailedException.php
@@ -1,1 +1,16 @@
-
+<?php
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/LaxovePHP/Encryption> Github Repository.
+ * @license <https://github.com/LaxovePHP/Encryption/blob/master/LICENSE> New BSD License.
+ */
+ 
+ namespace Nenglish7\Lovell\Exception;
+ 
+ /**
+  * KeyCreationFailed.
+  */
+ class KeyCreationFailedException extends RuntimeException implements ExceptionInterface
+ {
+ }
