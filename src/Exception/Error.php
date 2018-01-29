6bc884bdc0117fc63efd84c69e3a85d14559c551
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 15:43:58 -0500

    Update Error.php

--- src/Exception/Error.php
+++ src/Exception/Error.php
@@ -1,1 +1,27 @@
-
+<?php
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/Lovell> Github Repository.
+ * @license <https://github.com/Nenglish7/Lovell/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Nenglish7\Lovell\Exception;
+ 
+/**
+ * Error.
+ */
+class Error extends RuntimeException implements ExceptionInterface
+{
+  
+    /**
+     * Constuct a custom error exception.
+     *
+     * @return void.
+     */
+    function __construct()
+    {
+        parent::__construct('`An unknown error occured` - `100`');
+    }
+ 
+}
