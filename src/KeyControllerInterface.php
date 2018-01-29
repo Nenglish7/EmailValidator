39d4774682fe38d24884f6d909dcf7ebb2efb94b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 14:06:13 -0500

    Create KeyControllerInterface.php

---
+++ src/KeyControllerInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/Lovell> Github Repository.
+ * @license <https://github.com/Nenglish7/Lovell/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Nenglish7\Lovell;
+
+/**
+ * KeyControllerInterface.
+ */
+interface KeyControllerInterface
+{
+}
