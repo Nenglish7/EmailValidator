8e058ba0510aa11d187109f911a18158bf31fb9a
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 13:49:24 -0500

    Create RuntimeException.php

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/LaxovePHP/Encryption> Github Repository.
+ * @license <https://github.com/LaxovePHP/Encryption/blob/master/LICENSE> New BSD License.
+ */
+ 
+ namespace Nenglish7\Encryption\Exception;
+ 
+ /**
+  * RuntimeException.
+  */
+ class RuntimeException extends \RuntimeException implements ExceptionInterface
+ {
+ }
