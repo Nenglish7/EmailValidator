26c9a167b7a018417f57e8b55cbfd8ff9a730279
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-29 14:08:07 -0500

    Update RuntimeException.php

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -2,8 +2,8 @@
 /**
  * @author Nicholas English <nenglish0820@outlook.com>.
  *
- * @link    <https://github.com/LaxovePHP/Encryption> Github Repository.
- * @license <https://github.com/LaxovePHP/Encryption/blob/master/LICENSE> New BSD License.
+ * @link    <https://github.com/Nenglish7/Lovell> Github Repository.
+ * @license <https://github.com/Nenglish7/Lovell/blob/master/LICENSE> New BSD License.
  */
  
  namespace Nenglish7\Lovell\Exception;
